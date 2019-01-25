e08205fdcf6666a82686e21fcf55cee2db9602a4
Andrade <andradejr-93@hotmail.com>
2019-01-25 13:33:06 -0300

    Add find_answer.py

---
+++ find_answer.py
@@ -0,0 +1,61 @@
+"""Crawler to find the answer."""
+from requests_html import HTMLSession, HTML
+
+
+def decode_token(token):
+    """Method to decode the token.
+
+    Args:
+        token (str): encoded token extracted from html content.
+    Returns:
+        decoded_token: decoded token.
+    """
+    replacements = {'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v',
+                    'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q',
+                    'k': 'p', 'l': 'o', 'm': 'n', 'n': 'm', 'o': 'l',
+                    'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g',
+                    'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b',
+                    'z': 'a'}
+    decoded_token = ''
+    for digit in token:
+        if replacements.get(digit):
+            decoded_token += replacements.get(digit)
+        else:
+            decoded_token += digit
+    return decoded_token
+
+
+def find_answer(url='http://applicant-test.us-east-1.elasticbeanstalk.com/'):
+    """Method to get the answer.
+
+    Args:
+        url (str): url of the site with the answer.
+    Returns:
+        answer (str): answer found in the site.
+        status_code (int): number of the status code of the request.
+    """
+    with HTMLSession() as session:
+        r = session.get(url)
+        token = get_token(r.content.decode('utf-8'))
+        token = decode_token(token)
+        data = {"token": token}
+        response = session.post(url, data=data)
+        return response.content.decode('utf-8'), response.status_code
+
+
+def get_token(content):
+    """Method to get the token.
+
+    Args:
+        content (str): text content of the html request.
+    Returns:
+        token (str): token extracted from html content.
+    """
+    html = HTML(html=content)
+    token = html.find('input', first=True).attrs.get('value')
+    return token
+
+if __name__ == '__main__':
+    answer, status_code = find_answer()
+    print(f'Status code: {status_code}')
+    print(f'Answer: {answer}')
